9bb524018f826c2ae8cec1200b1e7098b7c6735a
nahuel di santi <nahuel.disanti@gmail.com>
2023-04-20 20:25:32 -0300

    ejs repair

--- app/server.js
+++ app/server.js
@@ -20,7 +20,8 @@ const app = express();
 //app.use(passport.session())
 
 app.set('view engine', 'ejs');
-app.set('views', './public/views');
+app.set('views', './public/views/partials');
+app.set('views', path.join(__dirname, './public/views/partials'));
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
